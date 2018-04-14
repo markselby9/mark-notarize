420c86abe755e9f2d96baaa4d0e999ca4c0f0d16
Mark Feng <markselbyfcy@gmail.com>
2018-04-14 17:30:38 +0800

    Add blockchainService.js to interact with contracts and call some methods

---
+++ src/components/NewNotarize.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import { addNotarizeInFactoryInstance, getDeployedNotarizesInFactoryInstance,
+  getNotarizeByAddress, isFinishedInNotarizeInstance } from '../api/blockchainService';
+
+class ListNotarize extends Component {
+  state = {
+    result: null,
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleClickGet = this.handleClickGet.bind(this);
+    this.handleClickAdd = this.handleClickAdd.bind(this);
+    this.fetchInstanceOnAddress = this.fetchInstanceOnAddress.bind(this);
+  }
+
+  componentWillMount() {
+
+  }
+
+  handleClickGet() {
+    console.log('handleClickGet');
+    getDeployedNotarizesInFactoryInstance(response => {
+      console.log(response);
+      this.setState({
+        result: JSON.stringify(response),
+      })
+    });
+  }
+
+  handleClickAdd() {
+    console.log('handleClickAdd');
+    addNotarizeInFactoryInstance({
+      givenUserA: this.usera.value || 0,
+      givenUserB: this.userb.value || 0,
+      givenContent: this.content.value || 0,
+      givenValidDays: 30
+    }, (response) => {
+      this.setState({
+        result: 'added',
+      });
+    })
+  }
+
+  fetchInstanceOnAddress() {
+    isFinishedInNotarizeInstance(this.itemAddress.value, response => {
+      console.log(response);
+      alert(response);
+    });
+  }
+
+  render() {
+    const { result } = this.state;
+    return (
+      <div>
+        <section>
+          <label>
+            usera address: <input type="text" ref={(usera) => this.usera = usera}/>
+          </label><br/>
+          <label>
+            userb address: <input type="text" ref={(userb) => this.userb = userb}/>
+          </label><br/>
+          <label>
+            content: <input type="text" ref={(content) => this.content = content}/>
+          </label><br/>
+          <button onClick={this.handleClickAdd}>add an item</button>
+          <button onClick={this.handleClickGet}>get deployed notarize list</button>
+        </section>
+        <section>
+          result: {result}
+        </section>
+        <section>
+          <label>
+            Item address: <input type="text" ref={(itemAddress) => this.itemAddress = itemAddress}/>
+          </label><br/>
+          <button onClick={this.fetchInstanceOnAddress}>is instance finished?</button>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default ListNotarize;
--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -21,7 +21,7 @@ let getWeb3 = new Promise(function(resolve, reject) {
     } else {
       // Fallback to localhost if no web3 injection. We've configured this to
       // use the development console's port by default.
-      var provider = new Web3.providers.HttpProvider('http://localhost:7545')
+      var provider = new Web3.providers.HttpProvider('http://0.0.0.0:7545')
 
       web3 = new Web3(provider)
 
